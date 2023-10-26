686d56c3aa0fa2592c3b5388ca37223f0bcaa402
han sangho <sangho0407@gmail.com>
2023-10-26 15:26:58 +0900

    study

---
+++ 2.Programmers/Lv0/영어가싫어요.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    num_list = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ] 
+    
+    for idx,num in enumerate(num_list) :
+        numbers = numbers.replace(num,str(idx))
+    return int(numbers)
